31d51e6bc5a81b9a99b5047995c80664c9068179
Saurabh Gupta <saurabg@evadon-mbp.DHCP.thefacebook.com>
2016-12-16 01:13:03 -0800

    Added timer
    Added timer

--- cmpe277_lab3/AddController.swift
+++ cmpe277_lab3/AddController.swift
@@ -10,7 +10,39 @@ import UIKit
 
 class AddController: UIViewController {
     var result = 0
+    var n1 = 0
+    var n2 = 0
+    var qNumber = 0
+    var ans = String()
+    var expectedAns = Int()
 
+    @IBOutlet weak var number1: UILabel!
+    @IBOutlet weak var number2: UILabel!
+    @IBOutlet weak var quesNumber: UILabel!
+    @IBOutlet weak var answer: UITextField!
+    @IBOutlet weak var timeRem: UILabel!
+    
+    var counter = 5
+    var timer = Timer()
+    
+    //function called on action : editing changed
+    @IBAction func ansChanged(_ sender: UITextField) {
+        NSLog("Value Change method called for text field")
+        ans = answer.text!
+        if (Int(ans) == n1 + n2) {
+            NSLog("Correct answer came in text field")
+            result = result + 1
+            if(qNumber < 10) {
+                prepareNextQuestion()
+            } else {
+                showResultAlertDialog()
+            }
+        } else {
+            NSLog("Waiting for correct answer");
+        }
+    }
+    
+    
     //action handler when back button is pressed from the navigation bar
     @IBAction func backPrompt(_ sender: UIBarButtonItem) {
         showQuitAlertDialog()
@@ -18,6 +50,7 @@ class AddController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareNextQuestion()
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,7 +59,7 @@ class AddController: UIViewController {
     
     //dummy function to check the functionality of the result button
     @IBAction func resultButton(_ sender: UIButton) {
-        showResultAlertDialog()
+        prepareNextQuestion()
     }
     
     
@@ -49,5 +82,68 @@ class AddController: UIViewController {
         }))
         present(talert, animated: true, completion: nil)
     }
+    
+    //function to generate the random numbers between 0-9
+    func generateRandomNumber() -> Int{
+        var RandomNumber = arc4random() % 10
+        RandomNumber += 1
+        return Int(RandomNumber)
+    }
+    
+    //function to display the next question on the screen, quesNumber and set the expected Ans in model
+    func prepareNextQuestion(){
+        
+        n1 = generateRandomNumber()
+        n2 = generateRandomNumber()
+            
+        counter = 5
+        timeRem.text = String(counter)
+        
+        answer.text=""
+            
+        var n1_s = String()
+        n1_s = String(n1)
+            
+        var n2_s = String()
+        n2_s = String(n2)
+            
+        number1.text = n1_s
+        number2.text = n2_s
+            
+        expectedAns = calcResult(n1: n1, n2: n2)
+            
+        qNumber = qNumber + 1
+        quesNumber.text = String(qNumber)
+        
+        //startTimer()
+    }
+    
+    //func to return the result based on the numbers and operation
+    func calcResult(n1:Int, n2:Int) ->Int {
+        return n1+n2
+    }
+    
+    //function to start the timer
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.3, target: self, selector: #selector(timerAction), userInfo: nil,repeats: true)
+    }
+
+    //function to decrease the timer tick
+    func timerAction() {
+        counter = counter - 1
+        timeRem.text = String(counter)
+    
+        if(counter == 0)
+        {
+            timer.invalidate()
+            counter = 5
+            if(qNumber < 10) {
+                prepareNextQuestion()
+            } else {
+                showResultAlertDialog()
+            }
+        }
+    }
+
 }
 
