6f1207cb152a0dd2b028649ec1511524b9658a6b
Saurabh Gupta <saurabg@evadon-mbp.DHCP.thefacebook.com>
2016-12-16 13:20:58 -0800

    Adding the subtraction controller
    Adding the subtraction controller

--- cmpe277_lab3/SubController.swift
+++ cmpe277_lab3/SubController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class SubController: UIViewController {
+class SubController: UIViewController ,KeyboardDelegate{
     var result = 0
+    var n1 = 0
+    var n2 = 0
+    var qNumber = 0
+    var ans = String()
+    var expectedAns = Int()
+    
+
+    @IBOutlet weak var number1: UILabel!
+    @IBOutlet weak var timeRem: UILabel!
+    @IBOutlet weak var quesNumber: UILabel!
+    @IBOutlet weak var answer: UITextField!
+    @IBOutlet weak var number2: UILabel!
+    
+    var counter = 5
+    var timer = Timer()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        prepareNextQuestion()
+        initKeyboard()
     }
     
     override func didReceiveMemoryWarning() {
@@ -44,5 +60,125 @@ class SubController: UIViewController {
         }))
         present(talert, animated: true, completion: nil)
     }
+    
+    //function to generate the random numbers between 0-9
+    func generateRandomNumber() -> Int{
+        var RandomNumber = arc4random() % 10
+        RandomNumber += 1
+        return Int(RandomNumber)
+    }
+    
+    //function to display the next question on the screen, quesNumber and set the expected Ans in model
+    func prepareNextQuestion(){
+        
+        //generated number should be single digit
+        n1 = generateRandomNumber()
+        while(n1 >= 10) {
+            n1 = generateRandomNumber()
+        }
+        
+        n2 = generateRandomNumber()
+        while(n2 >= 10 || n2 > n1) {
+            n2 = generateRandomNumber()
+        }
+        
+        answer.text=""
+        
+        var n1_s = String()
+        n1_s = String(n1)
+        
+        var n2_s = String()
+        n2_s = String(n2)
+        
+        number1.text = n1_s
+        number2.text = n2_s
+        
+        expectedAns = calcResult(n1: n1, n2: n2)
+        
+        qNumber = qNumber + 1
+        quesNumber.text = String(qNumber)
+        
+        timer.invalidate()
+        counter = 5
+        timeRem.text = String(counter)
+        startTimer()
+    }
+    
+    //func to return the result based on the numbers and operation
+    func calcResult(n1:Int, n2:Int) ->Int {
+        return n1+n2
+    }
+    
+    //function to start the timer
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.3, target: self, selector: #selector(timerAction), userInfo: nil,repeats: true)
+    }
+    
+    //function to decrease the timer tick
+    func timerAction() {
+        counter = counter - 1
+        timeRem.text = String(counter)
+        
+        if(counter == 0)
+        {
+            timer.invalidate()
+            counter = 5
+            if(qNumber < 10) {
+                prepareNextQuestion()
+            } else {
+                counter = 0
+                showResultAlertDialog()
+            }
+        }
+    }
+    
+    func initKeyboard() {
+        let keyboardView = Keyboard(frame: CGRect(x: 0, y: 0, width: 0, height: 300))
+        keyboardView.delegate = self
+        answer.inputView = keyboardView
+        answer.becomeFirstResponder()
+    }
+    
+    func keyWasTapped(character: String) {
+        NSLog(character)
+        if(character == "ENTER"){
+            NSLog("Enter is pressed")
+            ans = answer.text!
+            
+            if (Int(ans) != n1 + n2) {
+                if(qNumber < 10) {
+                    prepareNextQuestion()
+                } else {
+                    timer.invalidate()
+                    showResultAlertDialog()
+                }
+            } else {
+                //if correct answer came in field, it was handled by text field listener
+            }
+            
+        } else {
+            NSLog("Number key is pressed")
+            answer.insertText(character)
+        }
+    }
+    
+    
+    @IBAction func ansChanged(_ sender: UITextField) {
+        NSLog("Value Change method called for text field")
+        ans = answer.text!
+        if (Int(ans) == n1 + n2) {
+            NSLog("Correct answer came in text field")
+            result = result + 1
+            if(qNumber < 10) {
+                prepareNextQuestion()
+            } else {
+                timer.invalidate()
+                showResultAlertDialog()
+            }
+        } else {
+            NSLog("Waiting for correct answer");
+        }
+    }
+    
 }
 
